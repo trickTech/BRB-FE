9256c13bad6fb8448f13ad0616c5da4f9a31b00a
echaoo <zmk181@gmail.com>
2016-12-03 21:40:59 +0800

    添加about内容

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
+  <div class="about-container">
     <h1>关于本应用</h1>
-    <p>123455661231</p>
-
+    <p>号外号外,期待已久的红黑榜终于出炉啦!</p>
+    <br>
+    <p>红榜,用来表扬在学校遇见的好人好事；<br>黑榜,用来批评在学校遇见的烦心事儿。</p>
+    <p>(对事不对人哦同学!)</p>
+    <p class="copyright">版权所有: 飞鸢科技</p>
   </div>
 </template>
-<style>
+<style lang="less" scoped>
+  .about-container {
+    margin-top: 20px;
+    text-align: center;
+
+    h1 {
+      font-size: 20px;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-family: "微软雅黑", serif;
+    }
+
+    .copyright {
+      margin-top: 50px;
+    }
+  }
 </style>
 <script>
   export default{}
