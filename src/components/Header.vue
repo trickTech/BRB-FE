353798ba518900f4dfc5afdfc924d5daaa89667a
sky-ADMIN <546485299@qq.com>
2016-12-03 17:59:28 +0800

    CSS reset 和header

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>石大红黑榜</h1>
+    <div>
+      <button class="learn-more">了解更多</button>
+      <button class="publish">我要发布</button>
+    </div>
+  </header>
+</template>
+<style lang="less" scoped>
+header {
+  margin-top: 0;
+  background-color: #C73E3A;
+  h1 {
+    text-align: center;
+    font-size: 18px;
+    font-family: 微软雅黑,serif;
+    margin: 0;
+    padding: 8px 0 3px 0;
+  }
+  button {
+    border: none;
+    background: none;
+    margin: 0px 5px 8px 5px;
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 3px 6px;
+  }
+  .publish {
+    float: right;
+  }
+  .learn-more {
+
+  }
+}
+</style>
+<script>
+  export default{}
+</script>
