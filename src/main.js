3c16bcc6b4ed175e8ebe78fd2da1e23217770a95
sky-ADMIN <546485299@qq.com>
2016-12-03 16:38:40 +0800

    添加VueResource依赖

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+Vue.http.options.credentials = true
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  http: {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  },
   template: '<App/>',
   components: { App }
 })
