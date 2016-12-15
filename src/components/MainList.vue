ad2b1582b4173020bba23d1e60c16ccfe006bc75
sky-ADMIN <546485299@qq.com>
2016-12-15 09:04:10 +0800

    暂存

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -29,29 +29,27 @@
 <script>
   import API from '../config/requestConfig'
   import getQueryString from '../util/getQueryString'
+  // import $ from 'jquery'
   export default{
     data () {
       return {
         type: 1,
-        list: []
+        list: [],
+        isLogin: false
       }
     },
     mounted () {
       this.getData()
+      this.getIsLogin().then(function () {
+
+      })
       if (this.getIsLogin()) {
         console.log(123)
       } else {
-        // if ()
-        // window.location.href = 'https://openapi.yiban.cn/oauth/authorize?client_id=07f11a3f2773e24e&redirect_uri=http://f.yiban.cn/iapp28401&display=html'
-        // console.log(getQueryString('verify_request'))
-        // console.log(getQueryString('yb_uid'))
         if (getQueryString('verify_request') === null) {
           window.location.href = 'https://openapi.yiban.cn/oauth/authorize?client_id=07f11a3f2773e24e&redirect_uri=http://f.yiban.cn/iapp28401&display=html'
         } else {
-          let postData = {
-            msg: getQueryString('verify_request')
-          }
-          this.$http.post(API.auth, postData).then(function (res) {
+          this.$http.post(API.auth, {'verify_request': getQueryString('verify_request')}).then(function (res) {
             console.log(res)
           })
         }
@@ -68,6 +66,7 @@
       getIsLogin () {
         this.$http.get(API.isLogin).then(function (res) {
           console.log(res.data)
+
         })
       }
     }
--- src/config/requestConfig.js
+++ src/config/requestConfig.js
@@ -2,12 +2,14 @@
  * Created by lhy95 on 2016/12/2.
  */
 
-let baseUrl = 'http://115.159.184.76:23333/'
+// let baseUrl = 'http://115.159.184.76:23333/'
+let baseUrl = process.env.baseUrl
 const requestConfig = {
   postEvent: baseUrl + 'events/',
   getEvent: baseUrl + 'events/',
-  isLogin: baseUrl + 'auth/islogin',
-  auth: baseUrl + 'auth/auth'
+  isLogin: baseUrl + 'auth/islogin/',
+  auth: baseUrl + 'auth/auth/',
+  goYibanOauth: ''
 }
 
 module.exports = requestConfig
