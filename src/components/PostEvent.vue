d04dd60bc291ae59b790bc4c38a8a0e32134f045
echaoo <zmk181@gmail.com>
2016-12-17 15:43:28 +0800

    添加输入检测和提交失败处理

--- src/components/PostEvent.vue
+++ src/components/PostEvent.vue
@@ -9,63 +9,71 @@
       <input type="radio" v-model="event_type" value="1">黑榜
     </div>
     <button type="button" v-on:click="submitEvent" class="submit">提交</button>
-    <button type="button" v-on:click="goback" class="back">返回主页面</button>
+    <button type="button" v-on:click="goback" class="submit" style="margin-top: 10px">返回主页面</button>
+    <p v-show="failed" class="error">{{errorMsg}}</p>
   </div>
 </template>
 <style lang="less" scoped>
-  .event-content{
+  .event-content {
     text-align: center;
     font-size: 16px;
     color: buttonface;
     padding-top: 60px;
 
-    .title{
-      width: 90%;
-      height: 35px;
-      margin: 20px 0;
-      outline: none;
-    }
+  .title {
+    width: 90%;
+    height: 35px;
+    margin: 20px 0;
+    outline: none;
+  }
 
-    .content{
-      width: 90%;
-      height: 100px;
-      outline: none;
-      resize: none;
-    }
+  .content {
+    width: 90%;
+    height: 100px;
+    outline: none;
+    resize: none;
+  }
 
-    .typeradio{
-      margin-top: 20px;
-    }
+  .typeradio {
+    margin-top: 20px;
+  }
 
-    .submit{
-      width: 90%;
-      border-radius: 0;
-      height: 40px;
-      margin-top: 20px;
-      outline: none;
-      border: none;
-      border: 1px solid #ffffff;
-    }
+  .submit {
+    width: 90%;
+    border-radius: 0;
+    height: 40px;
+    margin-top: 20px;
+    outline: none;
+    border: none;
+    border: 1px solid #ffffff;
+  }
 
-    .back{
-      color: #666;
-      font-size: 12px;
-      margin: 10px;
-      border: none;
-      background-color: #ffffff;
-      outline: none;
-    }
+  .error {
+    color: red;
+    margin-top: 20px;
+  }
+
+  .back {
+    color: #666;
+    font-size: 12px;
+    margin: 10px;
+    border: none;
+    background-color: #ffffff;
+    outline: none;
+  }
+
   }
 </style>
 <script>
-  // import HeaderComponent from './components/header.vue'
   import API from '../config/requestConfig'
   export default{
     data () {
       return {
         title: '',
         content: '',
-        event_type: null
+        event_type: null,
+        failed: false,
+        errorMsg: ''
       }
     },
     methods: {
@@ -75,15 +83,32 @@
           content: this.content,
           event_type: parseInt(this.event_type)
         }
+        let that = this
+
+        if (!this.checkForm()) {
+          that.failed = true
+          this.errorMsg = '您输入的数据有误,请检查后重新提交'
+          return
+        }
         this.$http.post(API.postEvent, postData).then(function (res) {
           console.log(res)
           this.$router.push('/')
+        }, function (respose) {
+          console.log(respose)
+          that.failed = true
+          this.errorMsg = '网络异常'
         })
+      },
+      checkForm () {
+        if (this.title !== '' && this.content !== '' && this.event_type !== null) {
+          return true
+        } else {
+          return false
+        }
       },
       goback () {
         this.$router.push('/')
       }
-    },
-    components: {}
+    }
   }
 </script>
